1d240bddd6f3be3170d4642ef657232c93739c0d
eng0waleed <eng.0waleed@gmail.com>
2023-12-02 01:34:29 +0300

    updated python serial

--- utilities/image-processing/serial.py
+++ utilities/image-processing/serial.py
@@ -2,8 +2,18 @@ import numpy as np
 from PIL import Image
 import os
 import math
+from numba import cuda
 
-def rgb2lms(r, g, b):
+# Define process types as integer constants
+SIMULATE_PROTANOPIA = 1
+SIMULATE_DEUTERANOPIA = 2
+SIMULATE_TRITANOPIA = 3
+CORRECT_PROTANOPIA = 4
+CORRECT_DEUTERANOPIA = 5
+CORRECT_TRITANOPIA = 6
+
+
+def serial_rgb2lms(r, g, b):
     rr = r / 255.0
     gg = g / 255.0
     bb = b / 255.0
@@ -21,8 +31,7 @@ def rgb2lms(r, g, b):
     return l, m, s
 
 
-
-def lms2rgb(l, m, s):
+def serial_lms2rgb(l, m, s):
     rr = (l * 5.47221206) + (m * -1.1252419) + (s * 0.02980165)
     gg = (l * -4.6419601) + (m * 2.29317094) + (s * -0.19318073)
     bb = (l * 0.16963708) + (m * -0.1678952) + (s * 1.16364789)
@@ -31,47 +40,53 @@ def lms2rgb(l, m, s):
     gg = 12.92 * gg if gg <= 0.0031308 else 1.055 * gg ** (1 / 2.4) - 0.055
     bb = 12.92 * bb if bb <= 0.0031308 else 1.055 * bb ** (1 / 2.4) - 0.055
 
-    rr = np.clip(rr * 255, 0, 255)
-    gg = np.clip(gg * 255, 0, 255)
-    bb = np.clip(bb * 255, 0, 255)
+    rr = rr * 255
+    gg = gg * 255
+    bb = bb * 255
+
+    # Manually implement clipping
+    rr = 0 if rr < 0 else (255 if rr > 255 else rr)
+    gg = 0 if gg < 0 else (255 if gg > 255 else gg)
+    bb = 0 if bb < 0 else (255 if bb > 255 else bb)
+
     return int(rr), int(gg), int(bb)
 
-# Define the simulate and correct functions for protanopia, deuteranopia, and tritanopia
 
-def simulate_protanopia(l, m, s):
+# Define the simulate and correct functions for protanopia, deuteranopia, and tritanopia
+def serial_simulate_protanopia(l, m, s):
     ll = (0.0 * l) + (2.02344 * m) + (-2.52581 * s)
     mm = (0.0 * l) + (1.0 * m) + (0.0 * s)
     ss = (0.0 * l) + (0.0 * m) + (1.0 * s)
-    rr, gg, bb = lms2rgb(ll, mm, ss)
+    rr, gg, bb = serial_lms2rgb(ll, mm, ss)
     return rr, gg, bb
 
-def simulate_deuteranopia(l, m, s):
+def serial_simulate_deuteranopia(l, m, s):
     ll = (1.0 * l) + (0.0 * m) + (0.0 * s)
     mm = (0.494207 * l) + (0.0 * m) + (1.24827 * s)
     ss = (0.0 * l) + (0.0 * m) + (1.0 * s)
-    rr, gg, bb = lms2rgb(ll, mm, ss)
+    rr, gg, bb = serial_lms2rgb(ll, mm, ss)
     return rr, gg, bb
 
-def simulate_tritanopia(l, m, s):
+def serial_simulate_tritanopia(l, m, s):
     ll = (1.0 * l) + (0.0 * m) + (0.0 * s)
     mm = (0.0 * l) + (1.0 * m) + (0.0 * s)
     ss = (-0.395913 * l) + (0.801109 * m) + (0.0 * s)
-    rr, gg, bb = lms2rgb(ll, mm, ss)
+    rr, gg, bb = serial_lms2rgb(ll, mm, ss)
     return rr, gg, bb
 
 # Define correct functions for each type of color vision deficiency
 
-def correct_protanopia(l, m, s):
+def serial_correct_protanopia(l, m, s):
     ll = (0.0 * l) + (2.02344 * m) + (-2.52581 * s)
     mm = (0.0 * l) + (1.0 * m) + (0.0 * s)
     ss = (0.0 * l) + (0.0 * m) + (1.0 * s)
 
     # Color correction logic specific to protanopia
     # Convert back to rgb
-    rr, gg, bb = lms2rgb(ll, mm, ss)
+    rr, gg, bb = serial_lms2rgb(ll, mm, ss)
 
     # get the rgb
-    r, g, b = lms2rgb(l, m, s)
+    r, g, b = serial_lms2rgb(l, m, s)
 
     # Calculate error
     rr = r - rr
@@ -90,17 +105,17 @@ def correct_protanopia(l, m, s):
 
     return r, g, b
 
-def correct_deuteranopia(l, m, s):
+def serial_correct_deuteranopia(l, m, s):
     ll = (1.0 * l) + (0.0 * m) + (0.0 * s)
     mm = (0.494207 * l) + (0.0 * m) + (1.24827 * s)
     ss = (0.0 * l) + (0.0 * m) + (1.0 * s)
 
     # Color correction logic specific to deuteranopia
     # Convert back to rgb
-    rr, gg, bb = lms2rgb(ll, mm, ss)
+    rr, gg, bb = serial_lms2rgb(ll, mm, ss)
 
     # Calculate error
-    error_r, error_g, error_b = lms2rgb(l, m, s)
+    error_r, error_g, error_b = serial_lms2rgb(l, m, s)
 
     # Shift the color towards visible spectrum and add compensation
     r = error_r
@@ -114,17 +129,17 @@ def correct_deuteranopia(l, m, s):
 
     return r, g, b
 
-def correct_tritanopia(l, m, s):
+def serial_correct_tritanopia(l, m, s):
     ll = (1.0 * l) + (0.0 * m) + (0.0 * s)
     mm = (0.0 * l) + (1.0 * m) + (0.0 * s)
     ss = (-0.395913 * l) + (0.801109 * m) + (0.0 * s)
 
     # Color correction logic specific to tritanopia
     # Convert back to rgb
-    rr, gg, bb = lms2rgb(ll, mm, ss)
+    rr, gg, bb = serial_lms2rgb(ll, mm, ss)
 
     # Calculate error
-    error_r, error_g, error_b = lms2rgb(l, m, s)
+    error_r, error_g, error_b = serial_lms2rgb(l, m, s)
 
     # Shift the color towards visible spectrum and add compensation
     r = error_r
@@ -139,19 +154,31 @@ def correct_tritanopia(l, m, s):
     return r, g, b
 
 # Function to process image for a specific color vision deficiency
-def process_image(image_path, process_function, output_path):
+def process_image(image_path, process_type, output_path):
     image = Image.open(image_path)
     image_data = np.array(image)
 
     for i in range(image_data.shape[0]):
         for j in range(image_data.shape[1]):
             r, g, b = image_data[i, j]
-            l, m, s = rgb2lms(r, g, b)
-            ll, mm, ss = process_function(l, m, s)
+            l, m, s = serial_rgb2lms(r, g, b)
+            if process_type == SIMULATE_PROTANOPIA:
+                ll, mm, ss = serial_simulate_protanopia(l, m, s)
+            elif process_type == SIMULATE_DEUTERANOPIA:
+                ll, mm, ss = serial_simulate_deuteranopia(l, m, s)
+            elif process_type == SIMULATE_TRITANOPIA:
+                ll, mm, ss = serial_simulate_tritanopia(l, m, s)
+            elif process_type == CORRECT_PROTANOPIA:
+                ll, mm, ss = serial_correct_protanopia(l, m, s)
+            elif process_type == CORRECT_DEUTERANOPIA:
+                ll, mm, ss = serial_correct_deuteranopia(l, m, s)
+            elif process_type == CORRECT_TRITANOPIA:
+                ll, mm, ss = serial_correct_tritanopia(l, m, s)
             image_data[i, j] = [ll, mm, ss]
 
     output_image = Image.fromarray(image_data)
     output_image.save(output_path)
+
 
 def driver(image_file_name):
     base_image_path = f"images/{image_file_name}.bmp"
@@ -162,12 +189,12 @@ def driver(image_file_name):
         os.makedirs(out_directory)
 
     # Process for each type of color vision deficiency
-    process_image(base_image_path, simulate_protanopia, os.path.join(out_directory, f"simulate_protanopia.bmp"))
-    process_image(base_image_path, simulate_deuteranopia, os.path.join(out_directory, f"simulate_deuteranopia.bmp"))
-    process_image(base_image_path, simulate_tritanopia, os.path.join(out_directory, f"simulate_tritanopia.bmp"))
-    process_image(base_image_path, correct_protanopia, os.path.join(out_directory, f"correct_protanopia.bmp"))
-    process_image(base_image_path, correct_deuteranopia, os.path.join(out_directory, f"correct_deuteranopia.bmp"))
-    process_image(base_image_path, correct_tritanopia, os.path.join(out_directory, f"correct_tritanopia.bmp"))
+    process_image(base_image_path, SIMULATE_PROTANOPIA, os.path.join(out_directory, "simulate_protanopia.bmp"))
+    process_image(base_image_path, SIMULATE_DEUTERANOPIA, os.path.join(out_directory, "simulate_deuteranopia.bmp"))
+    process_image(base_image_path, SIMULATE_TRITANOPIA, os.path.join(out_directory, "simulate_tritanopia.bmp"))
+    process_image(base_image_path, CORRECT_PROTANOPIA, os.path.join(out_directory, "correct_protanopia.bmp"))
+    process_image(base_image_path, CORRECT_DEUTERANOPIA, os.path.join(out_directory, "correct_deuteranopia.bmp"))
+    process_image(base_image_path, CORRECT_TRITANOPIA, os.path.join(out_directory, "correct_tritanopia.bmp"))
 
 if __name__ == "__main__":
     driver("lena_color")
